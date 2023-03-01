6f186186b7a751c56e942662a7c7f2cae0e7c2d8
alexpineda99 <alexandropinedam1310@gmail.com>
2023-03-01 16:22:58 -0400

    aun problemas para subir imagenes

--- src/config/cloudinary_conf.js
+++ src/config/cloudinary_conf.js
@@ -1,8 +1,8 @@
-const cloudinary = require("cloudinary");
+const cloudinary = require("cloudinary").v2;
 
 const {CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET} = process.env;
 
-cloudinary.v2.config({
+cloudinary.config({
     cloud_name: CLOUDINARY_CLOUD_NAME,
     api_key: CLOUDINARY_API_KEY,
     api_secret: CLOUDINARY_API_SECRET,
--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,8 +1,24 @@
 const multer = require("multer");
 
-    const storage = multer.memoryStorage();
-  
-    const AvatarUpload = multer({storage}).single("file");
+// const storage = multer.memoryStorage();
 
-    export default AvatarUpload;
+// const singleUpload = multer({storage}).single("file");
+
+// export default singleUpload;
+
+const storageEngine = multer.diskStorage({
+    destination: "./images",
+    filename: (req, file, cb) => {
+      cb(null, `${Date.now()}--${file.originalname}`);
+    },
+  });
+  
+  const upload = multer({
+      storage: storageEngine
+  })
+  
+  
+  export default function uploadAvatar (file) {
+    upload.single(file)
+  } 
   
